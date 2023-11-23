6e33b14e572cad2ed5d10685c637fa3c621e7aa3
SOVANNDALINPHOEUK <151815318+SOVANNDALINPHOEUK@users.noreply.github.com>
2023-11-23 13:28:56 +0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,15 +1,16 @@
 public class recursion {
   public static void main(String[] args) {
 
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    System.out.println("5 to the power 2 is " + power(5,2));
+    System.out.println("6 to the power 3 is " + power(6,3));
+    System.out.println("7 to the power 4 is " + power(7,4));
+  
+    static double power(double x, int n) {
+      if (n != 0)
+         return (x * power(x, n - 1));
+      else
+         return 1;
     }
   
 }
-    
+}    
