2b6f8843d1a74882c129d9ff35492245890ba143
SOVANNDALINPHOEUK <151815318+SOVANNDALINPHOEUK@users.noreply.github.com>
2023-11-23 13:29:48 +0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -4,6 +4,7 @@ public class recursion {
     System.out.println("5 to the power 2 is " + power(5,2));
     System.out.println("6 to the power 3 is " + power(6,3));
     System.out.println("7 to the power 4 is " + power(7,4));
+    System.out.println("8 to the power 5 is " + power(8,5));
   
     static double power(double x, int n) {
       if (n != 0)
